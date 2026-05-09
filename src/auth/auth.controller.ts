0a613206ed72a26a8d55b4ecbe0f68ff09d45bb1
Ariel Aguilar <arielaguilar1994@gmail.com>
2026-05-09 15:16:56 +0200

    feat: implement basic message pattern

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,23 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // Implement 3 messagepattern 
-  /*
-    auth.register.user
-    auth.login.user
-    auth.verify.user
+    @MessagePattern('auth.register.user')
+    async authRegister(@Payload() user: any) {
+      return user;
+    }
 
-    se tienen que conetar al client-gateway pasando por nast y conectando con docker
-   */
+    @MessagePattern('auth.login.user')
+    async authLogin(@Payload() user: any) {
+      return user;
+    }
 
-
+    @MessagePattern('auth.verify.user')
+    async userVerify(@Payload() user: any) {
+      return user;
+    }
 }
