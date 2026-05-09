ed6486cc359a45fa538d320ac69ec221082f0278
Ariel Aguilar <arielaguilar1994@gmail.com>
2026-05-09 14:49:00 +0200

    wip: create micro env

---
+++ src/config/envs.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+  PORT: number;
+  NATS_SERVERS: string;
+}
+
+const envsSchema = joi.object({
+  PORT: joi.number().required(),
+  NATS_SERVER: joi.string().required()
+})
+.unknown(true); // le da la flexibilidad de tener mas definidas
+
+const { error, value } = envsSchema.validate( process.env );
+
+if (error) {
+  throw new Error(`Config validation error ${error.message}`);
+}
+
+const envsVars: EnvVars = value;
+
+export const envs = {
+  port: envsVars.PORT,
+  nats_servers: envsVars.NATS_SERVERS
+} 
